c269000ad04124197e6d51c555a87e33a8ad4d71
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2020-05-24 15:24:55 +0200

    Add files via upload

--- Aerial/My Project/AssemblyInfo.vb
+++ Aerial/My Project/AssemblyInfo.vb
@@ -9,10 +9,10 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("Aerial")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyCompany("")>
-<Assembly: AssemblyProduct("AerialTest")>
-<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyDescription("Claim desktop spaces")>
+<Assembly: AssemblyCompany("Chris Andriessen")>
+<Assembly: AssemblyProduct("Aerial")>
+<Assembly: AssemblyCopyright("Copyright © Chris Andriessen 2020")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("1.0.4.0")>
+<Assembly: AssemblyFileVersion("1.0.4.0")>
