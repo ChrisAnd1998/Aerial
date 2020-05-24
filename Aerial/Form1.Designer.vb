01af57dd054614e69e1cd0f1e0781033bd39e498
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2020-05-24 14:18:52 +0200

    Add files via upload

---
+++ Aerial/Form1.Designer.vb
@@ -0,0 +1,276 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox6 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox7 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox8 = New System.Windows.Forms.CheckBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(540, 466)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(124, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(152, 154)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(105, 157)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Primary"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(258, 154)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 4
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(364, 154)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 5
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(470, 154)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(470, 251)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 10
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(364, 251)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 9
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(258, 251)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox7.TabIndex = 8
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(152, 251)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox8.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(88, 254)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Secondary"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(152, 131)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(45, 17)
+        Me.CheckBox1.TabIndex = 16
+        Me.CheckBox1.Text = "Top"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(258, 131)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(59, 17)
+        Me.CheckBox2.TabIndex = 17
+        Me.CheckBox2.Text = "Bottom"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(364, 131)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(44, 17)
+        Me.CheckBox3.TabIndex = 18
+        Me.CheckBox3.Text = "Left"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(470, 131)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(51, 17)
+        Me.CheckBox4.TabIndex = 19
+        Me.CheckBox4.Text = "Right"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Location = New System.Drawing.Point(470, 228)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(51, 17)
+        Me.CheckBox5.TabIndex = 23
+        Me.CheckBox5.Text = "Right"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'CheckBox6
+        '
+        Me.CheckBox6.AutoSize = True
+        Me.CheckBox6.Location = New System.Drawing.Point(364, 228)
+        Me.CheckBox6.Name = "CheckBox6"
+        Me.CheckBox6.Size = New System.Drawing.Size(44, 17)
+        Me.CheckBox6.TabIndex = 22
+        Me.CheckBox6.Text = "Left"
+        Me.CheckBox6.UseVisualStyleBackColor = True
+        '
+        'CheckBox7
+        '
+        Me.CheckBox7.AutoSize = True
+        Me.CheckBox7.Location = New System.Drawing.Point(258, 228)
+        Me.CheckBox7.Name = "CheckBox7"
+        Me.CheckBox7.Size = New System.Drawing.Size(59, 17)
+        Me.CheckBox7.TabIndex = 21
+        Me.CheckBox7.Text = "Bottom"
+        Me.CheckBox7.UseVisualStyleBackColor = True
+        '
+        'CheckBox8
+        '
+        Me.CheckBox8.AutoSize = True
+        Me.CheckBox8.Location = New System.Drawing.Point(152, 228)
+        Me.CheckBox8.Name = "CheckBox8"
+        Me.CheckBox8.Size = New System.Drawing.Size(45, 17)
+        Me.CheckBox8.TabIndex = 20
+        Me.CheckBox8.Text = "Top"
+        Me.CheckBox8.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 398)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 100)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 24
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(676, 510)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.CheckBox5)
+        Me.Controls.Add(Me.CheckBox6)
+        Me.Controls.Add(Me.CheckBox7)
+        Me.Controls.Add(Me.CheckBox8)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Aerial"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents CheckBox7 As CheckBox
+    Friend WithEvents CheckBox8 As CheckBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Aerial/Form1.vb
@@ -0,0 +1,387 @@
+﻿Option Strict On
+
+Imports System.ComponentModel
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    Private Declare Function SHAppBarMessage Lib "shell32.dll" Alias "SHAppBarMessage" _
+    (ByVal dwMessage As Integer, <MarshalAs(UnmanagedType.Struct)> ByRef pData As _
+    APPBARDATA) As Integer
+
+
+
+    Private Structure APPBARDATA
+        Public cbSize As Integer
+        Public hWnd As IntPtr
+        Public uCallbackMessage As Integer
+        Public uEdge As Integer
+        Public rc As RECT
+        Public lParam As IntPtr
+    End Structure
+    Private Structure RECT
+        Public left As Integer
+        Public top As Integer
+        Public right As Integer
+        Public bottom As Integer
+    End Structure
+
+    Private Enum ABMsg As Integer
+        ABM_NEW = 0
+        ABM_REMOVE = 1
+        ABM_QUERYPOS = 2
+        ABM_SETPOS = 3
+        ABM_GETSTATE = 4
+        ABM_GETTASKBARPOS = 5
+        ABM_ACTIVATE = 6
+        ABM_GETAUTOHIDEBAR = 7
+        ABM_SETAUTOHIDEBAR = 8
+        ABM_WINDOWPOSCHANGED = 9
+        ABM_SETSTATE = 10
+    End Enum
+
+    Private Enum ABEdge As Integer
+        ABE_LEFT = 0
+        ABE_TOP = 1
+        ABE_RIGHT = 2
+        ABE_BOTTOM = 3
+    End Enum
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+
+    Sub reset()
+        For Each ff As Form In Application.OpenForms
+            Dim abd As New APPBARDATA
+            abd.cbSize = Marshal.SizeOf(abd)
+            abd.hWnd = ff.Handle
+
+            SHAppBarMessage(CType(ABMsg.ABM_REMOVE, Integer), abd)
+
+        Next
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        reset()
+
+
+        Dim allScreens = Screen.AllScreens
+
+
+
+        'Primary
+        If CheckBox1.Checked = True Then
+            For Each screen In allScreens
+                If screen.Primary = True Then
+                    Dim abd As New APPBARDATA
+                    abd.cbSize = Marshal.SizeOf(abd)
+
+
+
+                    Dim ff As New Form
+                    ff.AutoScaleBaseSize = New System.Drawing.Size(1, 1)
+                    ff.ClientSize = New System.Drawing.Size(100, 100)
+                    ff.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+                    ff.Opacity = 0
+                    ff.Name = "AerialSpacer"
+                    ff.ShowInTaskbar = False
+                    ff.Show()
+
+                    abd.hWnd = ff.Handle
+
+                    abd.uEdge = CInt(ABEdge.ABE_TOP)
+                    abd.rc.left = screen.WorkingArea.Left
+                    abd.rc.right = screen.WorkingArea.Right
+                    abd.rc.top = screen.WorkingArea.Top
+                    abd.rc.bottom = screen.WorkingArea.Bottom
+
+                    SHAppBarMessage(CType(ABMsg.ABM_NEW, Integer), abd)
+                    SHAppBarMessage(CInt(ABMsg.ABM_QUERYPOS), abd)
+
+
+                    abd.rc.bottom = abd.rc.top + CInt(TextBox1.Text)
+
+                    SHAppBarMessage(CInt(ABMsg.ABM_SETPOS), abd)
+
+                End If
+            Next
+        End If
+
+        If CheckBox2.Checked = True Then
+            For Each screen In allScreens
+                If screen.Primary = True Then
+                    Dim abd As New APPBARDATA
+                    abd.cbSize = Marshal.SizeOf(abd)
+
+
+
+                    Dim ff As New Form
+                    ff.AutoScaleBaseSize = New System.Drawing.Size(1, 1)
+                    ff.ClientSize = New System.Drawing.Size(100, 100)
+                    ff.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+                    ff.Opacity = 0
+                    ff.Name = "AerialSpacer"
+                    ff.ShowInTaskbar = False
+                    ff.Show()
+
+                    abd.hWnd = ff.Handle
+
+                    abd.uEdge = CInt(ABEdge.ABE_BOTTOM)
+                    abd.rc.left = screen.WorkingArea.Left
+                    abd.rc.right = screen.WorkingArea.Right
+                    abd.rc.top = screen.WorkingArea.Top
+                    abd.rc.bottom = screen.WorkingArea.Bottom
+
+                    SHAppBarMessage(CType(ABMsg.ABM_NEW, Integer), abd)
+                    SHAppBarMessage(CInt(ABMsg.ABM_QUERYPOS), abd)
+
+
+                    abd.rc.top = abd.rc.bottom - CInt(TextBox3.Text)
+
+
+                    SHAppBarMessage(CInt(ABMsg.ABM_SETPOS), abd)
+
+                End If
+            Next
+        End If
+
+        If CheckBox3.Checked = True Then
+            For Each screen In allScreens
+                If screen.Primary = True Then
+                    Dim abd As New APPBARDATA
+                    abd.cbSize = Marshal.SizeOf(abd)
+
+
+
+                    Dim ff As New Form
+                    ff.AutoScaleBaseSize = New System.Drawing.Size(1, 1)
+                    ff.ClientSize = New System.Drawing.Size(100, 100)
+                    ff.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+                    ff.Opacity = 0
+                    ff.Name = "AerialSpacer"
+                    ff.ShowInTaskbar = False
+                    ff.Show()
+
+                    abd.hWnd = ff.Handle
+
+                    abd.uEdge = CInt(ABEdge.ABE_LEFT)
+                    abd.rc.left = screen.WorkingArea.Left
+                    abd.rc.right = screen.WorkingArea.Right
+                    abd.rc.top = screen.WorkingArea.Top
+                    abd.rc.bottom = screen.WorkingArea.Bottom
+
+                    SHAppBarMessage(CType(ABMsg.ABM_NEW, Integer), abd)
+                    SHAppBarMessage(CInt(ABMsg.ABM_QUERYPOS), abd)
+
+
+                    abd.rc.right = abd.rc.left + CInt(TextBox4.Text)
+
+
+                    SHAppBarMessage(CInt(ABMsg.ABM_SETPOS), abd)
+
+                End If
+            Next
+        End If
+
+
+        If CheckBox4.Checked = True Then
+            For Each screen In allScreens
+                If screen.Primary = True Then
+                    Dim abd As New APPBARDATA
+                    abd.cbSize = Marshal.SizeOf(abd)
+
+
+
+                    Dim ff As New Form
+                    ff.AutoScaleBaseSize = New System.Drawing.Size(1, 1)
+                    ff.ClientSize = New System.Drawing.Size(100, 100)
+                    ff.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+                    ff.Opacity = 0
+                    ff.Name = "AerialSpacer"
+                    ff.ShowInTaskbar = False
+                    ff.Show()
+
+                    abd.hWnd = ff.Handle
+
+                    abd.uEdge = CInt(ABEdge.ABE_RIGHT)
+                    abd.rc.left = screen.WorkingArea.Left
+                    abd.rc.right = screen.WorkingArea.Right
+                    abd.rc.top = screen.WorkingArea.Top
+                    abd.rc.bottom = screen.WorkingArea.Bottom
+
+                    SHAppBarMessage(CType(ABMsg.ABM_NEW, Integer), abd)
+                    SHAppBarMessage(CInt(ABMsg.ABM_QUERYPOS), abd)
+
+
+                    abd.rc.left = abd.rc.right - CInt(TextBox5.Text)
+
+
+                    SHAppBarMessage(CInt(ABMsg.ABM_SETPOS), abd)
+
+                End If
+            Next
+        End If
+
+
+        'Secondary
+        If CheckBox8.Checked = True Then
+            For Each screen In allScreens
+                If screen.Primary = False Then
+                    Dim abd As New APPBARDATA
+                    abd.cbSize = Marshal.SizeOf(abd)
+
+
+
+                    Dim ff As New Form
+                    ff.AutoScaleBaseSize = New System.Drawing.Size(1, 1)
+                    ff.ClientSize = New System.Drawing.Size(100, 100)
+                    ff.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+                    ff.Opacity = 0
+                    ff.Name = "AerialSpacer"
+                    ff.ShowInTaskbar = False
+                    ff.Show()
+
+                    abd.hWnd = ff.Handle
+
+                    abd.uEdge = CInt(ABEdge.ABE_TOP)
+                    abd.rc.left = screen.WorkingArea.Left
+                    abd.rc.right = screen.WorkingArea.Right
+                    abd.rc.top = screen.WorkingArea.Top
+                    abd.rc.bottom = screen.WorkingArea.Bottom
+
+                    SHAppBarMessage(CType(ABMsg.ABM_NEW, Integer), abd)
+                    SHAppBarMessage(CInt(ABMsg.ABM_QUERYPOS), abd)
+
+                    abd.rc.bottom = abd.rc.top + CInt(TextBox8.Text)
+
+                    SHAppBarMessage(CInt(ABMsg.ABM_SETPOS), abd)
+
+                End If
+            Next
+        End If
+
+        If CheckBox7.Checked = True Then
+            For Each screen In allScreens
+                If screen.Primary = False Then
+                    Dim abd As New APPBARDATA
+                    abd.cbSize = Marshal.SizeOf(abd)
+
+
+
+                    Dim ff As New Form
+                    ff.AutoScaleBaseSize = New System.Drawing.Size(1, 1)
+                    ff.ClientSize = New System.Drawing.Size(100, 100)
+                    ff.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+                    ff.Opacity = 0
+                    ff.Name = "AerialSpacer"
+                    ff.ShowInTaskbar = False
+                    ff.Show()
+
+                    abd.hWnd = ff.Handle
+
+                    abd.uEdge = CInt(ABEdge.ABE_BOTTOM)
+                    abd.rc.left = screen.WorkingArea.Left
+                    abd.rc.right = screen.WorkingArea.Right
+                    abd.rc.top = screen.WorkingArea.Top
+                    abd.rc.bottom = screen.WorkingArea.Bottom
+
+                    SHAppBarMessage(CType(ABMsg.ABM_NEW, Integer), abd)
+                    SHAppBarMessage(CInt(ABMsg.ABM_QUERYPOS), abd)
+
+
+                    abd.rc.top = abd.rc.bottom - CInt(TextBox7.Text)
+
+
+                    SHAppBarMessage(CInt(ABMsg.ABM_SETPOS), abd)
+
+                End If
+            Next
+        End If
+
+
+        If CheckBox6.Checked = True Then
+            For Each screen In allScreens
+                If screen.Primary = False Then
+                    Dim abd As New APPBARDATA
+                    abd.cbSize = Marshal.SizeOf(abd)
+
+
+
+                    Dim ff As New Form
+                    ff.AutoScaleBaseSize = New System.Drawing.Size(1, 1)
+                    ff.ClientSize = New System.Drawing.Size(100, 100)
+                    ff.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+                    ff.Opacity = 0
+                    ff.Name = "AerialSpacer"
+                    ff.ShowInTaskbar = False
+                    ff.Show()
+
+                    abd.hWnd = ff.Handle
+
+                    abd.uEdge = CInt(ABEdge.ABE_LEFT)
+                    abd.rc.left = screen.WorkingArea.Left
+                    abd.rc.right = screen.WorkingArea.Right
+                    abd.rc.top = screen.WorkingArea.Top
+                    abd.rc.bottom = screen.WorkingArea.Bottom
+
+                    SHAppBarMessage(CType(ABMsg.ABM_NEW, Integer), abd)
+                    SHAppBarMessage(CInt(ABMsg.ABM_QUERYPOS), abd)
+
+
+                    abd.rc.right = abd.rc.left + CInt(TextBox6.Text)
+
+
+                    SHAppBarMessage(CInt(ABMsg.ABM_SETPOS), abd)
+
+                End If
+            Next
+        End If
+
+        If CheckBox5.Checked = True Then
+            For Each screen In allScreens
+                If screen.Primary = False Then
+                    Dim abd As New APPBARDATA
+                    abd.cbSize = Marshal.SizeOf(abd)
+
+
+
+                    Dim ff As New Form
+                    ff.AutoScaleBaseSize = New System.Drawing.Size(1, 1)
+                    ff.ClientSize = New System.Drawing.Size(100, 100)
+                    ff.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+                    ff.Opacity = 0
+                    ff.Name = "AerialSpacer"
+                    ff.ShowInTaskbar = False
+                    ff.Show()
+
+                    abd.hWnd = ff.Handle
+
+                    abd.uEdge = CInt(ABEdge.ABE_RIGHT)
+                    abd.rc.left = screen.WorkingArea.Left
+                    abd.rc.right = screen.WorkingArea.Right
+                    abd.rc.top = screen.WorkingArea.Top
+                    abd.rc.bottom = screen.WorkingArea.Bottom
+
+                    SHAppBarMessage(CType(ABMsg.ABM_NEW, Integer), abd)
+                    SHAppBarMessage(CInt(ABMsg.ABM_QUERYPOS), abd)
+
+
+                    abd.rc.left = abd.rc.right - CInt(TextBox5.Text)
+
+
+                    SHAppBarMessage(CInt(ABMsg.ABM_SETPOS), abd)
+
+                End If
+            Next
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        reset()
+    End Sub
+End Class
